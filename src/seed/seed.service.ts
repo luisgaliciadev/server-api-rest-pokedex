66818bbecc08200aa01e0a6ef4b5e0403d34a927
Luis Raul Galicia <luisgaliciadev@gmail.com>
2023-03-17 13:46:18 -0500

    Http adapter

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,24 +1,26 @@
 import { Injectable } from '@nestjs/common';
-import axios, { AxiosInstance } from 'axios';
+// import axios, { AxiosInstance } from 'axios';
 import { PokemonsResponse } from './interfaces/pokemons-response.model';
 import { CreatePokemonDto } from 'src/pokemon/dto/create-pokemon.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Pokemon } from '../pokemon/entities/pokemon.entity';
 import { Model } from 'mongoose';
+import { AxiosAdapter } from '../common/adapters/axios.adapters';
 
 @Injectable()
 export class SeedService {
-  private readonly axios: AxiosInstance = axios;
+  // private readonly axios: AxiosInstance = axios;
 
   constructor(
     @InjectModel(Pokemon.name)
     private readonly pokemonModel: Model<Pokemon>,
+    private readonly http: AxiosAdapter,
   ) {}
 
   async executeSeed() {
     await this.pokemonModel.deleteMany();
-    const { data } = await this.axios.get<PokemonsResponse>(
-      'https://pokeapi.co/api/v2/pokemon?limit=10',
+    const data = await this.http.get<PokemonsResponse>(
+      'https://pokeapi.co/api/v2/pokemon?limit=650',
     );
     // let pokemonPromise = [];
     // data.results.forEach(({ name, url }) => {
